7a62111aa5c14cf2942325330257fe319f383cf0
Vladislad Dr <despod96@mail.ru>
2023-03-22 20:32:45 +0300

    User input and first array

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+string value = Console.ReadLine();
+int size = Convert.ToInt32(value);
+
+int count = 0;
+string [] array = new string [size];
